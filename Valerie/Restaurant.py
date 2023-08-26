fdd4151aaafdf5a9f8aed2363b2fb031385e61f0
Valerie <valeriejeptoo4@gmail.com>
2023-08-26 16:57:28 +0300

    used the setter to set restaurant name

--- Valerie/Restaurant.py
+++ Valerie/Restaurant.py
@@ -1,8 +1,13 @@
-#created and initialized the class restaurant
-class Restaurant:
-    def __init__(self, name=None):
-        self._name = None
-        if name is not None:
-            self.set_name(name)  # Using the setter here
+#  
     
-   #
+    # Using the setter here to set name
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str):
+            self._name = name
+        else:
+            print("Name must a string!")
+    
+#
