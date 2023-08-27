60cc734e377a1df45ad5c2211e967ce45b0f57f4
Valerie <valeriejeptoo4@gmail.com>
2023-08-27 19:55:26 +0300

    added the rating property

--- Valerie/Review.py
+++ Valerie/Review.py
@@ -1,32 +1,28 @@
-#!/usr/bin/env python3
-
 class Review:
     # Class attribute to store all instances of Review
     all_reviews = []
 
-    def _init_(self, customer, restaurant, rating=None):
+    def __init__(self, customer, restaurant, rating=None):
         # Initialize instance attributes
         self.customer = customer
         self.restaurant = restaurant
-         # Initialize the private attribute _rating
+        # Initialize the private attribute _rating
         self._rating = None
-          # Adding the review to the all_reviews list
+        # Adding the review to the all_reviews list
         Review.all_reviews.append(self)
         if rating is not None:
             self.set_rating(rating)
-            # used to retrieve the value of a property
-            
-            def get_rating(self):
-             return self._rating    
-      
-         #used setter to ensure the rating is an interger
+
+    # Used to retrieve the value of the 'rating' property
+    def get_rating(self):
+        return self._rating
+
+    # Used setter to ensure the rating is an integer
     def set_rating(self, rating):
-        if isinstance (rating,int):
+        if isinstance(rating, int):
             self._rating = rating
-            
         else:
-            print("Rating must be an integer")  
-   
+            print("Rating must be an integer")
 
-
-    
+    # Create the 'rating' property using get_rating and set_rating methods
+    rating = property(get_rating, set_rating)
