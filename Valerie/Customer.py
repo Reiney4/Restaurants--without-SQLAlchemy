ce8198023399a32b542db76f4f01e171f896b465
Valerie <valeriejeptoo4@gmail.com>
2023-08-27 18:34:34 +0300

    initialized a customer instance

--- Valerie/Customer.py
+++ Valerie/Customer.py
@@ -1,11 +1,20 @@
  #!/usr/bin/env python3
 
-# Define a class Customer 
+# Define a class Customer  
 class Customer:
-    # Class attribute to store all instances of Customer
-    all_customers = []  
+    all_customers = []  # Class attribute to store all instances of Customer
     
-    #rs
+    # Initialize a customer instance with given_name and family_name
+    def _init_(self, given_name, family_name):
+        # Convert given_name to string
+        self.given_name = str(given_name) 
+         # Convert family_name to string 
+        self.family_name = str(family_name)
+        # Initialize full_name to None 
+        self.full_name = None
+        # Call the set_full_name method to set the full_name
+        self.set_full_name()
+        Customer.all_customers.append(self)  # Add the instance to the list of all customers
         
     # Set the given_name attribute
    #
