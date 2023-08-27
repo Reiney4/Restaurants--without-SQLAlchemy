f1ac79a817a6e77d3329947c7500a37622db4e02
Valerie <valeriejeptoo4@gmail.com>
2023-08-27 19:41:09 +0300

    used .append to add review to all reviews

--- Valerie/Review.py
+++ Valerie/Review.py
@@ -3,3 +3,16 @@
 class Review:
     # Class attribute to store all instances of Review
     all_reviews = []
+
+    def _init_(self, customer, restaurant, rating=None):
+        # Initialize instance attributes
+        self.customer = customer
+        self.restaurant = restaurant
+         # Initialize the private attribute _rating
+        self._rating = None
+          # Adding the review to the all_reviews list
+        Review.all_reviews.append(self)
+
+
+
+    
