10d885e878502a10ecb3de645c8a91e3dc1cf5d7
Valerie <valeriejeptoo4@gmail.com>
2023-08-31 00:35:58 +0300

    updated the files

--- Valerie/Customer.py
+++ Valerie/Customer.py
@@ -1,42 +1,87 @@
 #!/usr/bin/env python3
 
-# Define a class Customer
-class Customer:
-    all_customers = []  # Class attribute to store all instances of Customer
+# Assuming Review class is defined elsewhere
+class Review:
+    @staticmethod
+    def all():
+        pass
+  # Implement this method to return all reviews
 
-    # Initialize a customer instance with given_name and family_name
+class Customer:
+    all_customers = []
+
     def __init__(self, given_name, family_name):
-        # Convert given_name to string
-        self.given_name = given_name
-        # Convert family_name to string
-        self.family_name = family_name 
-        # Initialize full_name to None
-        self.full_name = None
-        # Call the set_full_name method to set the full_name
+        self._given_name = given_name
+        self._family_name = family_name
+        self._full_name = f"{given_name} {family_name}"
         self.set_full_name()
-        # Add the instance to the list of all customers from self
         Customer.all_customers.append(self)
 
-    # Set the given_name attribute
-        def given_name(self):
-            return self._given_name
-        def family_name(self):
-            return self._family_name
-        
-    
-    # Combine given_name and family_name to set the full_name attribute
+    def set_given_name(self, given_name):
+        if isinstance(given_name, str):
+            print(f"Setting given name to: {given_name}")
+            self._given_name = given_name
+            self.set_full_name()
+        else:
+            print('Given name must be a string.')
+
+    def get_given_name(self):
+         return self._given_name
+
+    given_name = property(get_given_name, set_given_name)
+
+    def set_family_name(self, family_name):
+        if isinstance(family_name, str):
+            print(f"Setting family name to: {family_name}")
+            self._family_name = family_name
+            self.set_full_name()
+        else:
+            print('Family name must be a string.')
+
+    def get_family_name(self):
+        return self._family_name
+
+    family_name = property(get_family_name, set_family_name)
+
     def set_full_name(self):
-        self.full_name = f"{self.given_name} {self.family_name}"    # Retrieve all customer instances
+        self._full_name = f"{self._given_name} {self._family_name}"
+
+    @property
+    def full_name(self):
+        return self._full_name
+
     @classmethod
     def all(cls):
         return cls.all_customers
 
+    def restaurants(self):
+        return set([review.restaurant() for review in Review.all() if review.customer() == self.full_name])
+
+    def add_review(self, restaurant, rating):
+         # Assuming the Review class constructor accepts these arguments
+        Review(self.full_name, restaurant, rating) 
+
+    def num_reviews(self):
+        reviews = [review for review in Review.all() if review.customer() == self.full_name]
+        return len(reviews)
+
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            if customer.full_name == name:
+                return customer
+            # Moved print statement outside the loop
+
+        print("Customer not found")  
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        return [customer for customer in cls.all_customers if customer.given_name == name]
+
 # Creating instances of the Customer class
-mycustomer = Customer("Valerie", "Kandagor")
-newcustomer = Customer("Vicky", "Kendy")
+my_customer = Customer("Valerie", "Kandagor")
+new_customer = Customer("Vicky", "Kendy")
 
 # Retrieve all customers and print their full names
 all_customers = Customer.all()
-#used loop to get all the names to be combined
 for customer in all_customers:
     print(customer.full_name)
--- Valerie/Restaurant.py
+++ Valerie/Restaurant.py
@@ -1,33 +1,65 @@
-#created and initialized the class restaurant
-class Restaurant:
-    def __init__(self, name):
-        #sets to private  remove none
-        self._name = name
-        
-    # Using the getter here to set name shd not change or @property
-    def name(self):
-        return self._name
-    
-    #a list of reviews should be returned
-    def reviews(self):
-        return [review for review in Review.all() if review.restaurant()== self._name]
-    
-    #a list of customersshould be returned
-    def customers(self):
-        return set([review.customer() for review in  self.reviews()] )
+class Review:
+    # Class attribute to store all instances of Review
+    all_reviews = []
 
-    #calculate the average string
-    def average_star_rating(self):
-        if not self.reviews():
-            return 0  # No reviews, so average is 0
-        total_ratings = sum(review.rating() for review in self.reviews())
-        average = total_ratings / len(self.reviews())
-        return average
-    
-# Create an instance of the Restaurant class
-restaurant = Restaurant("Sippin Serenade")
-print(restaurant.name())  
+    def __init__(self, customer, restaurant, rating=None):
+        # Initialize instance attributes
+        self.customer = customer
+        self.restaurant = restaurant
+        # Initialize the private attribute _rating
+        self._rating = None
+        # Adding the review to the all_reviews list
+        Review.all_reviews.append(self)
+        if rating is not None:
+            self.set_rating(rating)
 
-# name cannot be changed
-restaurant.name("Del Ruiz")
-print(restaurant.name()) 
+    # Used to retrieve the value of the 'rating' property
+    def rating(self):
+        return self._rating
+
+    # Used setter to ensure the rating is an integer
+    def set_rating(self, rating):
+        if isinstance(rating, int):
+            self._rating = rating
+        else:
+            print("Rating must be an integer")
+
+    # Create the 'rating' property using get_rating and set_rating methods
+    rating = property(rating, set_rating)
+
+    # returns customer
+    def customer(self):
+        return self._customer
+    
+    # returns restaurant
+    def restaurant(self):
+        return self._restaurant
+
+# use @classmethod decorator to define a method that operates on the class itself
+    @classmethod
+    #  the  method all() takes the class itself as the an argument(cls) method returns all reviews that have been created
+    def all(cls):
+        # created a variable with an empty list
+        printedReviews = []
+        for review in cls.all_reviews:
+            # append cause it has an empty list
+             printedReviews.append(f"Customer: {review.customer}, Rating: {review.rating}")
+
+        return printedReviews
+    
+    # creating an instance of the review class
+myreview = Review("Valerie Kandagor", "Sippin serenade", 19)
+# newreview = Review("Enrico Suarez", "Del Ruiz", " 20")
+
+# getting the ratings of the reviews using the rating() method
+print(myreview.rating)
+# print(newreview.rating)
+
+# getting all the reviews
+all_reviews = Review.all()
+print(all_reviews)
+
+
+
+    
+     
